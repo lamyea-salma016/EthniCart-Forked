83a0fc758d86ec2ebb7ec9beb46eb2485e0b25bd
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-08-02 20:50:59 +0600

    Template on privacy policy

---
+++ resources/views/privacy_policy.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'EthniCart | Privacy Policy')
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+@section('content')
+
+
+
+  
+@endsection
+
+
+
